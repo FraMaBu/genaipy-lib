0d68da3aafe06007cd6dcb4f77248672200ddd2b
FraMaBu <franz.buchmann07@gmail.com>
2023-10-28 17:50:20 +0200

    Initial commit

---
+++ prompts/summary_prompt.py
@@ -0,0 +1,28 @@
+""" Module for basic summary prompt template and prompt builder function"""
+
+SUMMARY_PROMPT_TEMPLATE = """
+Your task is to generate a summary of the text sample.
+Summarize the text sample provided below, delimited by triple backticks, in at most {max_words} words.
+
+Text sample:
+```{text}```
+
+Summarized text:
+"""
+
+
+def build_summary_prompt(
+    text: str, max_words: int = 150, template: str = SUMMARY_PROMPT_TEMPLATE
+) -> str:
+    """Build basic summary prompt from template and input.
+
+    Parameters:
+    - text: text sample to summarize.
+    - max_words: maximum number of summary words, by default 150.
+    - template: prompt template to use, must contain placeholders for all variables,
+                by default `SUMMARY_PROMPT_TEMPLATE`.
+
+    Returns:
+    - prepared prompt.
+    """
+    return template.format(text=text, max_words=max_words)
