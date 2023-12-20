bc3f7a521d0c363ccd57b275852c780364b927ea
FraMaBu <franz.buchmann07@gmail.com>
2023-12-20 12:39:29 +0100

    Added License and Readme

--- genaipy/prompts/generate_unittests.py
+++ genaipy/prompts/generate_unittests.py
@@ -1,7 +1,7 @@
 """Module for coding related prompt templates and system messages."""
 
 DEFAULT_SYS_MSG = """
-You are a world-class Python developer with an eagle eye for unintended bugs and edge cases. You carefully explain code with great detail and accuracy.
+You are a world-class Python developer with an eagle eye for unintended bugs and edge cases. You carefully explain and write code with great detail and accuracy.
 """
 
 EXPLAIN_FUN_TPL = """
@@ -9,15 +9,15 @@ Function:
 ```{function}```
 
 Instructions:
-Please explain the provided {language} function delimited by tripple back ticks.
+Explain the provided {language} function delimited by triple back ticks.
 Review what each element of the function is doing precisely and what the author's intentions may have been.
 
 Explanation:
 """
 
-PLAN_TEST_TPL = """
+PLAN_UNIT_TESTS_TPL = """
 Function:
-{function}
+```{function}```
 
 Guidelines:
 A good unit test suite should aim to:
@@ -28,5 +28,20 @@ A good unit test suite should aim to:
 - Be deterministic, so that the tests always pass or fail in the same way
 
 Instructions:
-Unit test the function above, list diverse scenarios that the function should be able to handle (and under each scenario, include a few examples as sub-bullets).
+Plan unit tests for the above function by listing diverse scenarios that the function should be able to handle.
+Include a few examples for each scenario.
+
+Cases:
+"""
+
+GENERATE_UNIT_TESTS_TPL = """
+Test cases:
+```{cases}```
+
+Instructions:
+Write a suite of unit tests for the function using Python and the `{unit_test_package}` package, following the above cases.
+Include helpful comments and docstrings to explain each test.
+Only return the test code in one code block in your answer.
+
+Tests:
 """
